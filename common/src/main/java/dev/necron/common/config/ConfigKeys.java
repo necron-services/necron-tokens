c67a52f7d302d1a03880d512895a392edd575ec4
KafeinTR <kafein40@gmail.com>
2022-05-14 23:06:11 +0300

    Initial commit

--- common/src/main/java/dev/necron/common/config/ConfigKeys.java
+++ common/src/main/java/dev/necron/common/config/ConfigKeys.java
@@ -12,7 +12,7 @@ public final class ConfigKeys {
 
         public static final ConfigKey<String> LANGUAGE = new ConfigKey<>("en", ConfigType.SETTINGS, "language");
 
-        public static final ConfigKey<String> ADMIN_PERM = new ConfigKey<>("sct.admin", ConfigType.SETTINGS, "admin-permission");
+        public static final ConfigKey<String> ADMIN_PERM = new ConfigKey<>("token.admin", ConfigType.SETTINGS, "admin-perm");
 
 
     }
@@ -20,9 +20,9 @@ public final class ConfigKeys {
     public static final class Language {
         private Language() {}
 
-        public static final ConfigKey<String> PREFIX = new ConfigKey<>("&8[&6SuperCombatTag&8]&r ", ConfigType.LANGUAGE, "prefix");
+        public static final ConfigKey<String> PREFIX = new ConfigKey<>("&6NecronToken &8➜", ConfigType.LANGUAGE, "prefix");
 
-        public static final ConfigKey<String> NO_PERMISSION = new ConfigKey<>("&cYou can't have permission to use this command!", ConfigType.LANGUAGE, "no-permission");
+        public static final ConfigKey<String> NO_PERMISSION = new ConfigKey<>("&cYou can't have permission to use this command!", ConfigType.LANGUAGE, "no-perm");
 
         public static final ConfigKey<String> NO_PLAYER = new ConfigKey<>("&cYou must be a player to use this command!", ConfigType.LANGUAGE, "no-player");
 
