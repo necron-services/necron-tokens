371c5a7cf46e0148b0dd01e141d74ab7b7426a68
KafeinTR <kafein40@gmail.com>
2022-05-14 23:41:45 +0300

    Added some listeners and events

---
+++ bukkit/src/main/java/dev/necron/bukkit/event/TokenReceiveEvent.java
@@ -0,0 +1,4 @@
+package dev.necron.bukkit.event;
+
+public class TokenReceiveEvent {
+}
---
+++ bukkit/src/main/java/dev/necron/bukkit/listener/EntityListener.java
@@ -0,0 +1,15 @@
+package dev.necron.bukkit.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class EntityListener implements Listener {
+
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onDeath(EntityDeathEvent event) {
+
+    }
+
+}
